7e43c0604192cdc09cbb79217755973575547571
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-10-31 21:00:09 -0400

    Created HolidayView ContentPage in .NET MAUI project. Added HolidayView.xaml and HolidayView.xaml.cs files to implement a ContentPage in the Canadian Holidays app.

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using CanadianHolidaysApp.Views;
+
+namespace CanadianHolidaysApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CanadianHolidaysApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
